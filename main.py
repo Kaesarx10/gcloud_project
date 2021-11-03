fa60038b00191d911c3dcab8850a91a70127aeb0
kaesarx10 <venzor.ca@gmail.com>
2021-11-03 01:00:31 -0500

    improved user interface

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def index():
     return "Congrats!"
 
+@app.route("/int:<celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
